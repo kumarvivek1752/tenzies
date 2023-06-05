c7bbb2d2b7434db62efa7dbc48a307b01dba85ae
Vivek Kumar <Kumarvivek1752@gmail.com>
2023-06-05 09:53:25 +0530

    added roll die

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import { nanoid } from 'nanoid';
 function App() {
   
 const [dice, setDice] = useState(allNewDice())
-
+function generateNewDie() {
+  return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+  }
+}
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-        newDice.push({
-            value: Math.ceil(Math.random() * 6), 
-          isHeld: false,
-          id: nanoid()
-        })
+        newDice.push(generateNewDie())
     }
     return newDice
   }
@@ -30,15 +32,21 @@ const [dice, setDice] = useState(allNewDice())
   }
 
 
-  function rollDice() { 
-    setDice(allNewDice());
-  }
+  function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ? 
+            die :
+            generateNewDie()
+    }))
+}
 
   const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={ ()=>holdDice(die.id) } />)
 
 
   return (
     <main className="App">
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
         {diceElements}
             </div>
